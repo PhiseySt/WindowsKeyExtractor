6a7f9533b729a36b86660709fde33be04ca5d3a9
FiseyskiySV <stanislav.fiseyskiy@akbarsdigital.ru>
2022-01-18 17:23:36 +0300

    First Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WindowsKeyExtractor
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TbPassword.Text = KeyExtractor.GetWindowsProductKey();
+        }
+    }
+}
